c5bc481389651dca84800a616f44ae99a41502b2
Tanagends <tanagends@gmail.com>
2024-02-19 01:02:40 +0200

    all

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
-"""
+"""Web app that renders states list html in alphabetical order"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Removes the current sqlalchemy session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_lis():
+    """renders a states list template"""
+    res = storage.all("State")
+    return render_template('7-states_list.html', res=res)
+
+
+if "__main__" == __name__:
+    app.run()
