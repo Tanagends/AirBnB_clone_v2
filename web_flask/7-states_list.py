87d570873dc067a95f4b60b37ce43da06ba813fd
Tanagends <tanagends@gmail.com>
2024-02-19 02:30:29 +0200

    flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""Web app that renders states list html in alphabetical order"""
+"""Importing Flask to run the web app"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """Render state_list html page to display States created"""
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
+
+
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Removes the current sqlalchemy session"""
+def teardown(self):
+    """Method to remove current SQLAlchemy Session"""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_lis():
-    """renders a states list template"""
-    ress = storage.all("State")
-    res = sorted(ress.values(), key=lambda state: state.name)
-    return render_template('7-states_list.html', res=res)
-
-
-if "__main__" == __name__:
-    app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
